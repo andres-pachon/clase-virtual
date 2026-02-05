0a46812589e1b60e32bb2126d4e21583a1b1b451
sdairon086 <sdaironalfoso@gmail.com>
2026-02-05 16:01:24 -0500

    porcentaje grasa corporal

--- Calculdora.java
+++ Calculdora.java
@@ -2,19 +2,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
 package calculdora;
+import java.util.Scanner;
 
 /**
  *
  * @author pacho
  */
+
+
+
 public class Calculdora {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Scanner sc = new Scanner(System.in);
+
+        double peso;
+        double altura;
+        int edad;
+        double imc;
+        double grasa;
+
+        System.out.print("Ingrese su peso en kg: ");
+        peso = sc.nextDouble();
+
+        System.out.print("Ingrese su altura en metros: ");
+        altura = sc.nextDouble();
+
+        System.out.print("Ingrese su edad: ");
+        edad = sc.nextInt();
+
+        // Calcular IMC
+        imc = peso / (altura * altura);
+
+        // Calcular porcentaje de grasa corporal (fórmula simple)
+        grasa = (1.2 * imc) + (0.23 * edad) - 10;
+
+        System.out.println("Porcentaje de grasa corporal: " + grasa + "%");
     }
-    
 }
+
+    
+
